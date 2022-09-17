547fa425fa60c6fe0d63bcec5acbe35f32d432e9
Ünal Parlak <unparlak@gmail.com>
2022-09-17 22:34:48 +0300

    delete and edit buttons are added

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,11 +1,18 @@
+import { useContext } from "react";
+import { Context } from "../Context";
+
 function Todo(props) {
+  const { handleEdit, handleDelete } = useContext(Context);
   return (
+    // nanoid hem id hem de key olacak yeni todolar için
+    // eskileri için key id olacak
     <div
       key="key"
       className={`todo__card ${props.isCompleted ? "completed" : "incomplete"}`}
     >
       <p>{props.content}</p>
-      <p>{props.isCompleted ? "true" : "false"}</p>
+      <button onClick={() => handleEdit(props.id)}>Edit</button>
+      <button onClick={() => handleDelete(props.id)}>Delete</button>
     </div>
   );
 }
